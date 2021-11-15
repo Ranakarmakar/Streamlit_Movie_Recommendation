3be8bdf6081888eb825c4e0354cb8170374594e8
Rana Karmakar <77492330+Ranakarmakar@users.noreply.github.com>
2021-11-15 13:38:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ vectorizer = TfidfVectorizer()
 feature_vectors = vectorizer.fit_transform(combined_features)
 similarity = cosine_similarity(feature_vectors)
 
-
+@st.cache(suppress_st_warning=True)
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(
         movie_id)
